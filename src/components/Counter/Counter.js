c16236e9911ac579f916cc062b03c0016d90ceae
Mike Turner <mike.turner@anu.edu.au>
2023-08-31 15:33:29 +0800

    Shows spinner for initial (server-side) render

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,15 +1,17 @@
 'use client';
 import React from 'react';
 
+import Spinner from '../Spinner';
+
 function Counter() {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(null);
 
   React.useEffect(() => {
     const savedCount = window.localStorage.getItem('saved-count');
     if (savedCount === null) {
       return;
     }
-    setCount(Number(savedCount));
+    setCount(savedCount ? Number(savedCount) : 0);
   }, []);
 
   React.useEffect(() => {
@@ -18,7 +20,7 @@ function Counter() {
 
   return (
     <button className="count-btn" onClick={() => setCount(count + 1)}>
-      Count: {count}
+      Count: {typeof count === 'number' ? count : <Spinner />}
     </button>
   );
 }
