2c12ae844c4b10a24a534a8f4947a70c1fe4178f
Mike Turner <mike.turner@anu.edu.au>
2023-09-01 09:34:55 +0800

    React-based solution to artist interview exercise

--- src/app/exercises/03-interview/page.js
+++ src/app/exercises/03-interview/page.js
@@ -6,27 +6,26 @@ import Interview from './Interview';
 import './styles.css';
 
 function InterviewExercise() {
+  const [displaySidebar, setDisplaySidebar] = React.useState(false);
   const isDesktop = useMediaQuery({
     query: '(min-width: 500px)',
   });
+  React.useEffect(() => {
+    setDisplaySidebar(isDesktop);
+  }, [isDesktop]);
 
   return (
     <main>
       <Interview />
-      {isDesktop && (
+      {displaySidebar && (
         <aside>
-          <img
-            src="/gwen-artist.png"
-            alt="Portrait of the artist"
-          />
+          <img src="/gwen-artist.png" alt="Portrait of the artist" />
           <h2>About the Artist</h2>
           <p>
-            Gwen Altaria is a contemporary artist known for
-            her unique blend of traditional oil painting
-            techniques and pop culture references. Born and
-            raised in London, England, Vivi had an early
-            love for both the fine arts and the fantastical
-            worlds of video games.
+            Gwen Altaria is a contemporary artist known for her unique blend of
+            traditional oil painting techniques and pop culture references. Born
+            and raised in London, England, Vivi had an early love for both the
+            fine arts and the fantastical worlds of video games.
           </p>
         </aside>
       )}
